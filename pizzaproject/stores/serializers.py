f60eb11fc39c4153a69bff411d46db0800c1bb82
YonatanGale <yonatangale2@gmail.com>
2023-05-14 21:27:02 -0400

    inicio

---
+++ pizzaproject/stores/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Pizzeria
+
+class PizzeriaListSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Pizzeria
+        fields = ['id', 'logo_image', 'pizzeria_name', 'city', 'zip_code',]
+
+class PizzeriaDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pizzeria
+        fields = ['id', 'pizzeria_name','street', 'city', 'state', 'zip_code', 'website', 'phone_number', 'description', 'logo_image', 'email', 'active']
---
+++ pizzaproject/stores/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [ 
+    path('', views.PizzeriaListAPIView.as_view(), name="pizzeria_list"), 
+    path('<int:id>/', views.PizzeriaRetrieveAPIView.as_view(), name='pizzeria_retrieve'),
+    path('update/<int:id>/', views.PizzeriaRetrieveUpdateAPIView.as_view(), name='pizzeria_update'),
+    path('delete/<int:id>/', views.PizzeriaDestroyAPIView.as_view(), name='pizzeria_delete'),
+    path('create/', views.PizzeriaCreateAPIView.as_view(), name='pizzeria_create'),
+    ]
